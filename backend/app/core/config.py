36ee0b23e5cf67c0b379233b6a62f048a13cf1e7
LouisVie61 <tantrieucao@gmail.com>
2026-05-12 21:20:44 +0700

    update the ttl of accessToken up to 7days

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 10080
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 10080 
 
     GOOGLE_API_KEY: str
 
